2688bbcca5b9081b07cd24d0321927a86ebe1c84
Dmitrj PRO <diman-pro@myprogram.us>
2015-10-06 20:17:36 +0300

    Исправил быструю сортировку

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
- 
+#include"DimanPRO.h"
 #include <cstdlib>
  
 using std::rand;
@@ -11,18 +11,29 @@ using std::swap;
 // следуют элементы меньшие pivot, а в конце - большие;
 // возвращает место начала блока элементов, больших pivot;
 int * partition(int * left, int *  right, int pivot) {
-	int * store = left; // место для вставки элементов, меньших pivot
-	for (int * p = left; p != right; ++p)
+	int * store = right; // место для вставки элементов, меньших pivot
+	while(true){
+		while (*left<pivot)
+			left++;
+		while (*right>pivot)
+			right--;
+		if (left<right){
+			swap(*left,*right);
+		} else {
+			return right;
+		}
+	}
+	/*for (int * p = left; p != right; ++p)
 		if (*p < pivot)
 			swap(*p, *store++);
-	return store;
+	return store;*/
 }
  
 void my_qsort(int * arr, int n) {
 	if (n <= 1)
 		return; // массив в 1 или 0 элементов уже упорядочен
-	int * pivotPtr = arr + rand() % n; // случайный выбор опорного элемента
+	int * pivotPtr = arr+rand() % n; // случайный выбор опорного элемента
 	int newPivotIdx = partition(arr, arr + n, *pivotPtr) - arr;
 	my_qsort(arr, newPivotIdx + 1);
-	my_qsort(arr + newPivotIdx, n - (newPivotIdx + 1));
+	my_qsort(arr + newPivotIdx+1, n - (newPivotIdx +1));
 }
